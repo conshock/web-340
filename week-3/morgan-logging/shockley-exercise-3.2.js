bee3cd72e1f3a66ca3181955acf64cdc29941970
conshock <conshockley@gmail.com>
2021-04-04 14:10:30 -0500

    Adding code comments to morgan exercise.

--- week-3/morgan-logging/shockley-exercise-3.2.js
+++ week-3/morgan-logging/shockley-exercise-3.2.js
@@ -1,10 +1,23 @@
+/*
+============================================
+; Title: Assignment 2.2
+; Author: Conner Shockley
+; Date: 4/3/21
+; Description: This program sets up a basic
+; Node webserver on port 3000 and adds
+; an ejs index page and a morgan logger.
+;===========================================
+*/
 
+// Standard require statements
 const express = require("express");
 const http = require("http");
 const path = require("path");
 const logger = require("morgan");
 
+// Start app.
 let app = express();
+
 
 app.set("views", path.resolve(__dirname, "views")); // Tell Express the views are in the 'views' directory
 app.set("view engine", "ejs"); // Tell Express to use the EJS view engine
@@ -20,6 +33,7 @@ app.get("/", function(req, res)
    });
 });
 
+// Initialize/create server
 http.createServer(app).listen(8080, function() {
     console.log("Application started on port 8080");
 });
