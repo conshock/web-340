7c02b59198fda58cb107cd4cb57a343d5b686823
conshock <conshockley@gmail.com>
2021-04-04 19:29:43 -0500

    Adding code comments.

--- week-3/putting-it-all-together/shockley-assignment3.4.js
+++ week-3/putting-it-all-together/shockley-assignment3.4.js
@@ -10,45 +10,52 @@
 ;===========================================
 */
 
-// Base require statements to start the app.
+// Base require statements for express, http, path, and Morgan logger.
 const express = require("express");
 const http = require("http");
 const path = require("path");
-
-//
 const logger = require("morgan");
 
+// Variable to initialize application.
 const app = express();
 
+// Setting up ejs view engine and pointing to the views folder for content.
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// Setup logger.
 app.use(logger("short"));
 
+// Each app.get sets up the message that will display for the respective page.
+// App.get for home/index page.
 app.get("/", function(request, response) {
     response.render("index", {
         message: "home page"
     });
 });
 
+// App.get for about page.
 app.get("/about", function(request, response) {
     response.render("about", {
         message: "about page"
     });
 });
 
+// App.get for contact page.
 app.get("/contact", function(request, response) {
     response.render("contact", {
         message: "contact page"
     })
 });
 
+// App.get for products page.
 app.get("/products", function(request, response) {
     response.render("products", {
         message: "products page"
     });
  });
 
+// Creating web server on port 8080.
 http.createServer(app).listen(8080, function () {
     console.log("Application started on port 8080");    
 });
