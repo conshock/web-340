b2d9649601f52b9213b5598d88a9c65dd8520ddb
conshock <conshockley@gmail.com>
2021-04-10 17:14:11 -0500

    Adding week-4 Exercise 4.2.

---
+++ week-4/json-api/shockley-exercise4.2.js
@@ -0,0 +1,29 @@
+// Require statements to make sure express, http, and morgan are going to run.
+let express = require('express');
+let http = require('http');
+let logger = require('morgan');
+
+
+// Variable to start the application.
+let app = express();
+
+// Variable to use Morgan logger.
+app.use(logger('dev'));
+
+
+// Variable to return JSON data based on the URL requested.
+app.get('/customer/:id', function(req, res) {
+    let id = parseInt(req.params.id, 10);
+
+    res.json({
+        firstName: 'Conner',
+        lastName: 'Shockley',
+        employeeId: id
+    });
+
+});
+
+// Starting HTTP server on port 3000.
+http.createServer(app).listen(3000, function() {
+    console.log('Application started and listening on port 3000')
+});
