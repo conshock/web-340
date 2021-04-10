c53c59ba807c5b1b1ba755aa3ead6bb8d2279cbe
conshock <conshockley@gmail.com>
2021-04-10 18:25:36 -0500

    Adding assignment 4.4

--- week-4/crud/shockley-assignment4.4.js
+++ week-4/crud/shockley-assignment4.4.js
@@ -1,35 +1,42 @@
 /*
 ============================================
-; Title:  app.js
-; Author: Professor Krasso
-; Date:   28 February 2019
+; Title:  shockley-assignment4.4.js
+; Author: Conner Shockley
+; Date:   April 10, 2021
 ; Description: Demonstrates CRUD operations in a
-;              Node.js API.
+; Node.js API.
 ;===========================================
 */
 
+// Require statements for Express and HTTP.
 const express = require("express");
 const http = require("http");
 
+// Setting app to express function.
 let app = express();
 app.set("port", process.env.PORT || 3000);
 
+// GET request.
 app.get("/", function(req, res) {
   res.send("API invoked as an HTTP GET request.");
 });
 
+// PUT request.
 app.put("/", function(req, res) {
   res.send("API invoked as an HTTP PUT request.");
 });
 
+// POST request.
 app.post("/", function(req, res) {
   res.send("API invoked as an HTTP POST request");
 });
 
+// DELETE request.
 app.delete("/", function(req, res) {
   res.send("API invoked as an HTTP DELETE request");
 });
 
+// Starting server on port 3000.
 http.createServer(app).listen(app.get("port"), function() {
   console.log(`Application started and listening on port ${app.get("port")}`);
 });
